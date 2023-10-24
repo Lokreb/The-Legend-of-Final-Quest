6d1bb56d0add992533814be1c264ac6f34e1e353
heris-fsya <fsya45@gmail.com>
2023-10-24 17:04:04 +0200

    {add} bouton fonctionel
    +system de combat
    lvl relier

--- Assets/_Scripts/BattleSystem.cs
+++ Assets/_Scripts/BattleSystem.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 public class BattleSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public BattleState state;
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
 
-    // Update is called once per frame
-    void Update()
+    Character_Stat playerstats;
+    Enemy_stat enemystats;
+
+    public TextMeshPro lvl;
+    public TextMeshPro Enemyname;
+
+        void Start()
     {
-        
+        state = BattleState.START;
+        SetupBattle();
+    }
+    void SetupBattle()
+    {
+        GameObject PlayerGO = Instantiate(playerPrefab);
+        PlayerGO.GetComponent<Character_Stat>();
+        GameObject EnemyGO = Instantiate(enemyPrefab);
+        EnemyGO.GetComponent<Enemy_stat>();
+        lvl.text = playerstats.level.ToString();
     }
 }
--- Assets/_Scripts/Character_Stat.cs
+++ Assets/_Scripts/Character_Stat.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Character_Stat : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string gender;
+
+    public int level;
+    public int damage;
+    public int maxHealth;
+    public int currentHealth;
+    public List<Sprite> sprites;
+    public double FinalDammage;
+    SpriteRenderer spriteRenderer;
+    
+
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (gender == "HOMME") 
+        {
+        spriteRenderer.sprite = sprites[0];
+        }
+        else if(gender == "FEMME")
+        {
+            spriteRenderer.sprite = sprites[1];
+        }
+        else if (gender == "NONE")
+        {
+            spriteRenderer.sprite = sprites[2];
+        }
+        level = 1;
+        damage = 10;
+        maxHealth = 100;
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    void CalculedDammage()
     {
-        
+        FinalDammage = level * 2 ^ level * damage;
     }
+
+    
 }
