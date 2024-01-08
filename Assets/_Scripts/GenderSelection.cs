9f66d67a36c968ac44ef6f05610c065b29c29b8e
Lokreb <85063467+Lokreb@users.noreply.github.com>
2024-01-08 20:09:29 +0100

    {Add} Scene intro avec choix du perso fonctionnel

--- Assets/_Scripts/GenderSelection.cs
+++ Assets/_Scripts/GenderSelection.cs
@@ -4,59 +4,49 @@ using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GenderSelection : MonoBehaviour, IPointerEnterHandler
+public class GenderSelection : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     public GameManager GM;
-    public Animator animatorMale;
-    public Animator animatorFemale;
-    public Animator animatorOther;
-    public GameObject self;
-    public GameObject game;
+    public Animator characterAnimator;
 
     public void SelectMale()
     {
         GM.saveGender(0);
         GM.LoadGender();
-        self.SetActive(false);
-        game.SetActive(true);
-
+        SceneManager.LoadScene("Map");
     }
 
     public void SelectFemale()
     {
         GM.saveGender(1);
         GM.LoadGender();
-        self.SetActive(false);
-        game.SetActive(true);
+        SceneManager.LoadScene("Map");
     }
 
     public void SelectOther()
     {
         GM.saveGender(2);
         GM.LoadGender();
-        self.SetActive(false);
-        game.SetActive(true);
-    }
-
-    public void PlayHoverMaleAnimation()
-    {
-        animatorMale.Play("Male");
+        SceneManager.LoadScene("Map");
     }
 
     public void OnPointerEnter(PointerEventData eventData)
-    {
-        Debug.Log("The cursor entered the selectable UI element.");
-    }
+     {
+         if (characterAnimator != null)
+         {
+             Debug.Log("Hey");
+             characterAnimator.SetTrigger("Walk");
+         }
+     }
 
-    public void PlayHoverFemaleAnimation()
-    {
-        animatorFemale.Play("Female");
-    }
-
-    public void PlayHoverOtherAnimation()
-    {
-        animatorOther.Play("Other");
-    }
+     public void OnPointerExit(PointerEventData eventData)
+     {
+         if (characterAnimator != null)
+         {
+             characterAnimator.SetTrigger("Idle");
+         }
+     }
 }
